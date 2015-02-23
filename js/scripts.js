1c32c8a71f7699b47442333f0ba7e505dff6b831
Razmechtalis <spartadreams@gmail.com>
2015-02-23 21:27:56 +0300

    Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 var button_search = document.querySelector(".search__btn");
 var popup = document.querySelector(".form-search");
-var hashtag = document.querySelector(".footer-page__hashtag");
 
 button_search.addEventListener("click", function(event) {
 	event.preventDefault();
-	popup.classList.add("is-visible");
-});
-
-hashtag.addEventListener("click", function(event) {
-	event.preventDefault();
-	console.log("Привет");
+	if (popup.classList.contains("is-visible")) {
+		popup.classList.remove("is-visible");
+	}
+	else {
+		popup.classList.add("is-visible");
+	}
 });
 
 window.addEventListener("keydown", function(event) {
@@ -25,7 +24,7 @@ function initialize() {
 		zoom: 9,
 		center: new google.maps.LatLng(34.867267, -111.760978)
 	}
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
 																mapOptions);
 	var myLatLng = new google.maps.LatLng(34.869661, -111.760949);
 	var beachMarker = new google.maps.Marker({
@@ -33,4 +32,4 @@ function initialize() {
 		map: map
 	});
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "load", initialize);
